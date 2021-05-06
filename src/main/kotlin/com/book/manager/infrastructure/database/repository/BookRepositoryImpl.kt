17ce2a0eead264ad4b1a88df37c7e89658f1a08f
Masashi Ogawa <masashi.ogawa@noin.tv>
2021-05-06 22:36:54 +0900

    add update book

--- src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
+++ src/main/kotlin/com/book/manager/infrastructure/database/repository/BookRepositoryImpl.kt
@@ -9,9 +9,11 @@ import com.book.manager.infrastructure.database.mapper.custom.BookWithRentalMapp
 import com.book.manager.infrastructure.database.mapper.custom.select
 import com.book.manager.infrastructure.database.mapper.custom.selectByPrimaryKey
 import com.book.manager.infrastructure.database.mapper.insert
+import com.book.manager.infrastructure.database.mapper.updateByPrimaryKeySelective
 import com.book.manager.infrastructure.database.record.BookRecord
 import com.book.manager.infrastructure.database.record.custom.BookWithRentalRecord
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 
 @Suppress("SpringJavaInjectionPointsAutowiringInspection")
 @Repository
@@ -29,6 +31,12 @@ class BookRepositoryImpl(
 
     override fun register(book: Book) {
         bookMapper.insert(toRecord(book))
+    }
+
+    override fun update(id: Long, title: String?, author: String?, releaseDate: LocalDate?) {
+        bookMapper.updateByPrimaryKeySelective(
+            BookRecord(id, title, author, releaseDate)
+        )
     }
 
     private fun toModel(record: BookWithRentalRecord): BookWithRental {
